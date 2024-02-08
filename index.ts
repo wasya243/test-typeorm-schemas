fa0edf9bf526d64434c818c2b0d55783a8c7a923
Vasyl <vasyl@voicett.com>
2024-02-08 17:25:01 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
+
+export {
+  User
+};
